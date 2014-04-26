12140464f347d3370d3aeb4720a2c7d7e1e19e00
David Saunders <david.saunders.design@gmail.com>
2014-04-26 14:57:01 -0400

    This is the final version of my Midterm Project.

--- _David_Saunders/midterm/lib/event.rb
+++ _David_Saunders/midterm/lib/event.rb
@@ -1,7 +1,5 @@
 require 'json'
 require 'rest-client'
-#require 'rubygems'
-#require 'nytimes-articles'
 
 #Event class.
 class Event
@@ -14,19 +12,4 @@ class Event
     @endpoint_url = "#{BASE_URL}.json?&query=#{event}&api-key=06c41782f4c87230d42fe7d14a1e288b:12:69215696"
   end
 
-  def initialize(name, borough)
-  	@name = name
-  	@borough = borough
-  end
 end
-
-  def get
-    response = RestClient.get(endpoint_url)
-    @parsed_response = JSON.parse(response)
-  end
-
-  def parsed_response
-  	@parsed_response['results'].map do |event|
-  		  { category: event['results']['category'], borough: event['results']['borough']}
-    end
-  end
--- _David_Saunders/midterm/midterm_runner.rb
+++ _David_Saunders/midterm/midterm_runner.rb
@@ -1,105 +1,41 @@
-## First, define our methods
+#This is David Saunders' Midterm Project for BEWD. 
+
 require_relative 'lib/event'
+require 'pry'
 
+def create_user
+  puts "Hi, there! Welcome to the NYC Events Finder. We'll help you find the perfect event!\n \nI'm David. What's your name?"
+  gets.strip.capitalize
 
-	puts "Hi, there! Welcome to the NYC events finder. We'll help you find the perfect event!\n \nWhat's your name, friend?"
-	name = gets.strip.capitalize
-	
+end
 
-	# puts "Sounds great! What borough are you in?"
-	# borough = gets.strip.capitalize
-
-
-
-def get_events_from_manhattan
-	res = JSON.load(RestClient.get('http://api.nytimes.com/svc/events/v2/listings.json?&filters=borough:"Manhattan"'))
-		res["results"].map do |event|
-			e = {borough:event["results"]["borough"]}
-			e 
-		end
+def get_events(borough)
+  	res = JSON.load(RestClient.get("http://api.nytimes.com/svc/events/v2/listings.json?&filters=borough:#{borough.capitalize}&api-key=06c41782f4c87230d42fe7d14a1e288b:12:69215696"))
+	res["results"].map do |event|
+		puts "#{event["event_name"]	}, #{event["venue_name"] }"
 	end
-
-def get_events_from_queens
-	res = JSON.load(RestClient.get('http://api.nytimes.com/svc/events/v2/listings.json?&filters=borough:"Queens"'))
-		res["results"].map do |event|
-			e = {borough:event["results"]["borough"]}
-			e 
-		end
-	end
+end
 
 
-def get_events_from_bronx
-	res = JSON.load(RestClient.get('http://api.nytimes.com/svc/events/v2/listings.json?&filters=borough:"Bronx"'))
-		res["results"].map do |event|
-			e = {borough:event["results"]["borough"]}
-			e 
-		end
-	end
+name = create_user
 
+puts "Awesome! OK, #{name}, let's get started.\n\nIn order to help you find the best event, we need to know where you are!\n\nWhat borough are you in? (M)anhattan? (Q)ueens? (B)rooklyn? Bron(x)? (S)taten Island?"
+borough = gets.strip.downcase
+#I switched to .downcase because the X in Bronx shouldn't be capitalized, and it threw out an error
 
-def get_events_from_brooklyn
-	res = JSON.load(RestClient.get('http://api.nytimes.com/svc/events/v2/listings.json?&filters=borough:"Brooklyn"'))
-		res["results"].map do |event|
-			e = {borough:event["results"]["borough"]}
-			e 
-		end
-	end
+case borough 
+	when "m"
+		puts get_events("Manhattan")
+	when "q"
+		puts get_events("Queens")
+	when "b"
+		puts get_events("Brooklyn")
+	when "x"
+		puts get_events("Bronx")
+	when "s"
+		puts get_events("Staten+Island")
+end
 
+#the Case method is much nicer than the if/else/elsif approach I was initially trying to make work. 
 
-def get_events_from_staten_island
-	res = JSON.load(RestClient.get('http://api.nytimes.com/svc/events/v2/listings.json?&filters=borough:"Staten20%Island"'))
-		res["results"].map do |event|
-			e = {borough:event["results"]["borough"]}
-			e 
-		end
-	end
-
-
-
-
-
-
-# def create_event
-# 	 puts "Awesome! What are you in the mood for?\nA (m)ovie? The (t)heatre? Maybe a (c)oncert?"
-# 	 event = gets.strip 
-
-####################################
-	
-
-	puts "Awesome! OK, #{name}, let's get started.\nWhat borough are you in? (M)anhattan? (Q)ueens? (B)rooklyn? Bron(x)? (S)taten Island?"
-	borough = gets.strip.capitalize
-
-		  if borough == "M"
-			puts get_events_from_manhattan
-
-		elsif borough == "Q"
-			puts get_events_from_queens
-
-		elsif borough == "B"
-			puts get_events_from_brooklyn
-
-		elsif borough == "X"
-			puts get_events_from_bronx
-			
-		else borough == "S"
-			puts get_events_from_staten_island
-
-	end
-
-
-create_user
-#event = create_event
-  
-
-# Now call methods to run the program
-
-
-
-
-# def show_results(event)
-# 	events.each do |event|
-#     		show_message "Category: #{event[:category]}, Borough: (#{event[:borough]})}"
-#  end
-# end
-
-
+puts "Wow! There sure are some great things happening!\n\nWe hope you have a great time :) Thanks for checking out NYC Events Finder"
